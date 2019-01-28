8fe87c967d518c701c1bacf4a8621a40a566c667
skaldat1 <skaldat1@binghamton.edu>
2019-01-27 21:13:50 -0500

    event listener added.

--- app/src/main/java/com/example/basic_android_app/MainActivity.java
+++ app/src/main/java/com/example/basic_android_app/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.basic_android_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonStart = findViewById(R.id.button_id_start);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                LottieAnimationView animationView = findViewById(R.id.animation_view);
+                animationView.resumeAnimation();
+            }
+        });
+
+        Button buttonStop = findViewById(R.id.button_id_stop);
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                LottieAnimationView animationView = findViewById(R.id.animation_view);
+                animationView.pauseAnimation();
+            }
+        });
     }
 }
